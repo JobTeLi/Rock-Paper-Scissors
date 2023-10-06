7b059600f6f1d1fe2bf390c4e76f0e4478d15add
JobTeLi <job.lindert@gmail.com>
2023-10-06 14:21:04 +0200

    changed the random numbers to rock paper scissor

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 //make function for computer choice
 function getComputerChoice() {
-//make a variable for Rock
-//make a variable for Paper
-//make a variable for Scissor
-//let the function pick one of the variables random
+
+//let the function choose between 0 and 3 random
   let randomCompChoice = Math.floor(Math.random() * 3) +1
-//
+//if picked 1 change to rock
+    if (randomCompChoice === 1) {
+      return "rock";
+}   else if (randomCompChoice === 2) {
+      return "paper";
+}   else {
+      return "scissor"
+}
+//if picked 2 change to paper
+//if picked 3 change to scissor
+//store choice in variable?
 //return random choice
-  return randomCompChoice;
+  return randomCompChoice();
 }
