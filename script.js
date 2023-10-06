b98b970c11826bb9eb3bbf41eb0caa80040c1af9
JobTeLi <job.lindert@gmail.com>
2023-10-06 14:07:06 +0200

    created the random picker for the game

---
+++ script.js
@@ -0,0 +1,11 @@
+//make function for computer choice
+function getComputerChoice() {
+//make a variable for Rock
+//make a variable for Paper
+//make a variable for Scissor
+//let the function pick one of the variables random
+  let randomCompChoice = Math.floor(Math.random() * 3) +1
+//
+//return random choice
+  return randomCompChoice;
+}
