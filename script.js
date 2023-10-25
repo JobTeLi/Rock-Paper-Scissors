c3d5dff304737502b52c050c37b1b743af760a47
JobTeLi <job.lindert@gmail.com>
2023-10-25 11:04:33 +0200

    added click event for buttons rock paper scissors and logic for displaying the results

--- script.js
+++ script.js
@@ -29,14 +29,14 @@ function playRound(playerSelection, computerSelection) {
         return "It's a Tie!";
     }
     else if (result === "Player") {
-        return `You win ${playerSelection} beats ${computerSelection}`
+        return "You win!"
     }
-    else {
-        return `You lose ${computerSelection} beats ${playerSelection}`
+    else if (result === "Computer") {
+        return "You lose!"
     }
 }
 
-function getPlayerChoice(){
+/*function getPlayerChoice(){
     let validatedInput = false;
     while(validatedInput == false) {
         const choice = prompt("Rock Paper Scissors");
@@ -50,9 +50,54 @@ function getPlayerChoice(){
         }
         
     }
-}
+}*/
+const results = document.querySelector(".result-round");
+const buttons = document.querySelectorAll('button');
+const player_Score = document.querySelector('#player-score');
+const computer_Score = document.querySelector('#computer-score');
+const Winner = document.querySelector('#Winner');
 
-function game() {
+const score = document.querySelector('.display-result');
+
+
+let computerScore = 0;
+let playerScore = 0;
+buttons.forEach((button) =>{
+    button.addEventListener('click', () => {
+        let result_round = document.createElement("li");
+        result_round.className = 'temp_result';
+
+        result_round.textContent = playRound(button.id,getComputerChoice())
+        results.appendChild(result_round);
+
+        if(result_round.textContent === "You win!"){
+            playerScore++;
+        }
+        else{
+            if(result_round.textContent === "You lose!"){
+                computerScore++;
+            }
+        }
+        if(computerScore === 5){
+            Winner.textContent = "Computer!";
+            buttons.forEach((button) => button.disabled = true);
+        }
+        else{
+            if(playerScore === 5){
+                Winner.textContent = "Player!";
+                buttons.forEach((button) => button.disabled = true);
+            }
+        }
+        player_Score.textContent = playerScore;
+        computer_Score.textContent = computerScore;
+        
+        
+    });
+});
+
+
+
+/*function game() {
     let playerScore = 0;
     let computerScore = 0;
     console.log("Lets play a game of Rock Paper Scissors!");
@@ -84,7 +129,7 @@ function game() {
         console.log("Its a tie");
     }
 
-}
-game();
+}*/
+//game();
 
 
